a50117e0d8cc52fc2719fe480b301fa473feab69
Kishore S <kishore@switchon.io>
2019-10-10 21:57:21 +0530

    Implementing the bubble sort.
    Implementing the bubble sort as a method in sorter class. The array can
    be passed to the class using constructor. sort method sorts the array in
    the same instance member 'collection' which is an array.
    
    Signed-off-by: Kishore S <kishore@switchon.io>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Initial checks!!');
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    // implementing the bubble sort here.
+    let { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          let temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+let sorter = new Sorter([10, 2, -1, 0]);
+sorter.sort();
+console.log(sorter.collection);
